b97f4c2d50dbad8393896e15ac3c1049aa96fa33
Mira Hedl <miroslav.hedl@avast.com>
2022-01-19 20:56:29 +0100

    WIP Wed Jan 19 20:56:29 CET 2022

--- jobs/bootstrap.groovy
+++ jobs/bootstrap.groovy
@@ -18,3 +18,25 @@ job('_LABS/create_lab3') {
       'DELETE')
   }
 }
+
+job('_LABS/create_lab4') {
+  parameters {
+    stringParam('REPO_JMENO')
+    stringParam('REPO_PRIJMENI')
+  }
+  scm {
+    git {
+      remote {
+        github("ds-jenkins-course/jobdsl")
+      }
+    }
+  }
+  steps {
+    dsl(
+            [
+                    'jobs/lab3/final_result.groovy',
+            ],
+            'DELETE')
+  }
+}
+
---
+++ jobs/lab4/final_result.groovy
@@ -0,0 +1,39 @@
+//def name = "mira"
+//def surname = "hedl"
+//
+def fldr_name = "_LABS/LAB4"
+
+folder(fldr_name)
+
+job("$fldr_name/final") {
+    description("""Generated solution for LAB 03 - Freestyle build \"Hello Jenkins\" aplikace""")
+    keepDependencies(false)
+    jdk("OpenJDK 8")
+    scm {
+        git {
+            remote {
+                github("jenkins-for-developers/$REPO_JMENO-$REPO_PRIJMENI-java", "https")
+            }
+            branch("**")
+        }
+    }
+    triggers {
+        githubPush()
+        scm("@daily") {
+        }
+    }
+
+    concurrentBuild(true)
+    steps {
+        maven {
+            goals('clean package')
+            mavenInstallation('Maven 3.3.9')
+            properties(skipTests: true)
+        }
+        shell("java -jar target/moje-apka-*.jar")
+    }
+    wrappers {
+        timestamps()
+        colorizeOutput()
+    }
+}
