a83ab1cba4bced19c3dcd0775c9c715997d270fd
fdolsky321 <fdolsky@gmail.com>
2022-01-20 06:18:48 +0100

    Oprava zavorky

--- jobs/lab3/final_result.groovy
+++ jobs/lab3/final_result.groovy
@@ -5,7 +5,7 @@ def fldr_name = "_LABS/LAB3"
 folder(fldr_name)
 
 job("$fldr_name/final") {
-    description("""<hr/ ><i>Generated solution for: <b>${desc}</b</i>><hr/ >""")
+    description("""<hr/ ><i>Generated solution for: <b>${desc}</b</i>><hr/ >"""
 
 "Generated solution for LAB3 - Building from GitHub")
     keepDependencies(false)
